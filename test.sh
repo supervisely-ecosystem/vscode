22a679f52544b3553b1253dadcbd42cf4ca61aaa
maxim kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-03 18:25:38 +0200

    fix config

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
-docker build --progress=plain -t supervisely/vscode:0.0.1 . && \
+# docker build --progress=plain -t supervisely/vscode:0.0.1 . && \
 # docker push supervisely/vscode:0.0.1 && \
 
 # -p <host port>:<container port>
-docker run --rm -it -p 3000:8000 -v $PWD/demo:/workdir supervisely/vscode:0.0.1
+# docker run --rm -it -p 3000:8000 -v $PWD/demo:/workdir supervisely/vscode:0.0.1
 # docker run --rm -it --entrypoint="" supervisely/vscode:0.0.1 bash
+
+
+docker run --rm -it -p 8000:8000 supervisely/vscode:0.0.1
