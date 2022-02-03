81870d2382dbfd889b83560dca83563507669619
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-03 17:55:43 +0200

    demo project (#1)
    * dockerimage wip
    
    * settings sync
    
    * test extensions
    
    * curl does not work
    
    * pylance issue
    
    * extensions work
    
    * extensions wip
    
    * pylance not working - wip
    
    * intellisense wip
    
    * stop
    
    * pyright wip
    
    * pyright wip
    
    * todos
    
    * todos
    
    * web-dev ideas
    
    * start opencv
    
    * wip
    
    * wip
    
    * sly sdk - wip
    
    * debug - wip
    
    * sly methods work
    
    * clean dockerfile
    
    * nginx works
    
    * settings - done
    
    * start demo app
    
    * start demo app
    
    * demo app
    
    * supervisely==6.7.0 wip
    
    * hello user works
    
    * first attempt

---
+++ test.sh
@@ -0,0 +1,6 @@
+docker build --progress=plain -t supervisely/vscode:0.0.1 . && \
+# docker push supervisely/vscode:0.0.1 && \
+
+# -p <host port>:<container port>
+docker run --rm -it -p 3000:8000 -v $PWD/demo:/workdir supervisely/vscode:0.0.1
+# docker run --rm -it --entrypoint="" supervisely/vscode:0.0.1 bash
