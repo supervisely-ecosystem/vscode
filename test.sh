1b05779f70db33c6fcaaf3ed33a3471f4c1a9650
maxim kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-04 08:30:47 +0200

    venv initialized by default

--- test.sh
+++ test.sh
@@ -6,10 +6,17 @@ docker build --progress=plain -t supervisely/vscode:0.0.1 . && \
 
 # docker run --rm -it --entrypoint="" supervisely/vscode:0.0.1 bash
 # -p <host port>:<container port>
-docker run --rm -it \
-    -p 8000:8000 \
-    # -v $PWD/demo:/workdir \
-    supervisely/vscode:0.0.1
+# docker run --rm -it \
+#     -p 8000:8000 \
+#     --entrypoint="" \
+#     supervisely/vscode:0.0.1 bash
+
+
+
+docker run --rm -it -p 8000:8000 supervisely/vscode:0.0.1
+# -v $PWD/demo:/workdir \
+
+
 
 #@TODO: 
 # vscode on /, app on /app (- / - return 200) + change uvicorn config
