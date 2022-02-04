b8f456445b8407d0cbc924fe803d3448f15403a2
maxim kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-04 18:10:48 +0200

    move todos to issues

--- test.sh
+++ test.sh
@@ -16,16 +16,3 @@ docker run --rm -it -p 8000:8000 \
     supervisely/vscode:0.0.1
 
 # -v $PWD/demo:/workdir \
-
-#@TODO: 
-
-# teamfiles-run from vscode from folder (os like dev environment - files + apps)
-# team files right click on agent gile - auto select correct agent in advanced settings (run app modal window)
-# todo: create startpage
-# how to save session configuration and restart it later?
-# modal window - simplify github integration
-# github tab - try connect github account (doesnt work)
-# if remove agent tocken from app container env
-# dont allow to run some apps on our agents in community
-# nginx w + w/o ///////?????
-# nginx silent mode
